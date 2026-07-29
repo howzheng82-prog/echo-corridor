6981e45a9b36ce0214d3ee84f0ce6aa4c534e5c2
howzheng82-prog <howzheng82@gmail.com>
2026-07-29 20:57:48 +0800

    Create WorldScene.js

---
+++ src/scene/WorldScene.js
@@ -0,0 +1,33 @@
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js';
+
+export class WorldScene{
+    constructor(game){
+        this.game = game;
+        this.mirrorMesh = null;
+    }
+
+    buildWorld(){
+        const scene = this.game.scene;
+        //光源
+        const light = new THREE.PointLight(0xffffff,0.6);
+        light.position.set(0,4,0);
+        scene.add(light);
+
+        //地面
+        const groundGeo = new THREE.PlaneGeometry(30,30);
+        const groundMat = new THREE.MeshStandardMaterial({color:0x222228});
+        const ground = new THREE.Mesh(groundGeo, groundMat);
+        ground.rotation.x = -Math.PI / 2;
+        scene.add(ground);
+
+        //镜子
+        const mirrorGeo = new THREE.PlaneGeometry(2.2,3.2);
+        const mirrorMat = new THREE.MeshStandardMaterial({
+            color:0x99ccff,
+            metalness:0.8
+        });
+        this.mirrorMesh = new THREE.Mesh(mirrorGeo, mirrorMat);
+        this.mirrorMesh.position.set(0,1.6,-8);
+        scene.add(this.mirrorMesh);
+    }
+}
