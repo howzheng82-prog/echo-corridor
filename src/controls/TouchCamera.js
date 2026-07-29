e6a3373db382900217b1217ef9ffe9dd11d9ecfb
howzheng82-prog <howzheng82@gmail.com>
2026-07-29 20:57:16 +0800

    Create TouchCamera.js

---
+++ src/controls/TouchCamera.js
@@ -0,0 +1,22 @@
+export class TouchCamera{
+    constructor(game){
+        this.game = game;
+        this.viewArea = document.getElementById("touchViewArea");
+        this.lastX = 0;
+        this.yaw = 0;
+        this.speed = 0.0042;
+
+        this.bindTouch();
+    }
+
+    bindTouch(){
+        this.viewArea.addEventListener("touchstart", e=>{
+            this.lastX = e.touches[0].clientX;
+        })
+        this.viewArea.addEventListener("touchmove", e=>{
+            const dx = e.touches[0].clientX - this.lastX;
+            this.yaw += dx * this.speed;
+            this.lastX = e.touches[0].clientX;
+        })
+    }
+}
